4f1f64b8545b50925a0362b85635e0695eb0bd35
Kris Moore <kris@ixsystems.com>
2019-08-02 09:50:08 -0400

    Initial add of truenas subdir for building images, only adds ability to setup / update repos right now

---
+++ truenas/build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+. ./CONF.sh
+
+exit_err() {
+	echo "$@"
+	exit 1
+}
+
+case $1 in
+	truenasmirror)
+		scripts/update-mirror truenas
+		;;
+	debmirror)
+		scripts/update-mirror debian
+		;;
+	clean)
+		make -C ../ distclean
+		;;
+	*)
+		exit_err "Invalid option selected"
+		;;
+esac
