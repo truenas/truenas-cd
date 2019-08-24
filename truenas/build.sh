f95c03448d5e18865ff17e7d40b7ed30651bf565
Kris Moore <kris@ixsystems.com>
2019-08-24 13:24:34 -0400

    Dynamically set the ZFSMOD version/string at CD creation time

--- truenas/build.sh
+++ truenas/build.sh
@@ -7,11 +7,22 @@ exit_err() {
 	exit 1
 }
 
+set_zfs_mod() {
+	ZFSMOD=$(ls /srv/aptly-publish/truenas/unstable/pool/main/z/zfs-dkms-bin/ | grep zfs-modules | head -n 1 | cut -d '_' -f 1)
+	if [ -z "$ZFSMOD" ] ; then
+		echo "ERROR locating zfs-modules package"
+		exit 1
+	fi
+	sed -i '' "s|%%ZFSMOD%%|${ZFSMOD}|g" ../tasks/truenas-truenas-essential
+	sed -i '' "s|%%ZFSMOD%%|${ZFSMOD}|g" ../truenas/conf/preseed.cfg
+}
+
 case $1 in
 	clean)
 		make -C ../ distclean
 		;;
 	iso)
+		set_zfs_mod
 		make -C ../ packagelists
 		make -C ../ image-trees
 		make -C ../ images
